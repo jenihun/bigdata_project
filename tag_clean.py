8ac3c0264737bd100272a4925fa24f34c3bb3188
jenihun <ghkddnjs1014@naver.com>
2023-11-10 22:57:55 +0900

    first commit

---
+++ tag_clean.py
@@ -0,0 +1,62 @@
+data = {
+    "nations": [
+        {
+            "nation_name": "Korea",
+            "food": ["Kimchi", "Bulgogi", "Bibimbap", "Tteokbokki", "Samgyeopsal", "Doenjang Jjigae", "Naengmyeon", "Bulgogi", "Galbi", "Hotteok"],
+            "culture": ["Hanbok", "K-Pop", "Kimchi-making", "Taekwondo", "Gugak", "Korean Calligraphy", "Hanji Crafts", "Confucianism", "Korean Tea Ceremony"],
+            "structure": ["Gyeongbokgung Palace", "Changdeokgung Palace", "Bulguksa Temple", "Seokguram Grotto", "N Seoul Tower", "Dongdaemun Design Plaza", "Lotte World Tower", "Incheon International Airport", "Jeju Stone Park", "Hwaseong Fortress"],
+            "city_name": ["Seoul", "Busan", "Incheon", "Daegu", "Daejeon", "Gwangju", "Ulsan", "Suwon"]
+        },
+        {
+            "nation_name": "Saudi Arabia",
+            "food": ["Kabsa", "Mandi", "Shawarma", "Mutabbaq", "Harees", "Jareesh", "Samboosa", "Falafel", "Ful Medames", "Murtabak"],
+            "culture": ["Arabian Coffee (Gahwa)", "Traditional Clothing (Thobe/Abaya)", "Camel Racing", "Falconry", "Traditional Music (Sawt and Ardah)", "Al-Sirah Al-Nabawiyyah (Islamic Heritage)", 
+                        "Souks (Traditional Markets)", "Desert Culture", "Henna Art", "Traditional Dance (Alardah)"],
+            "structure": ["Masmak Fortress", "Abraj Al Bait Towers", "Kingdom Centre Tower", "Qasr Al-Hukm", "King Fahd Fountain", "Masjid al-Haram", "Al-Masmak Palace", 
+                          "Kingdom Centre Tower", "Al Faisaliah Center", "King Abdulaziz Center for World Culture"],
+            "city_name": ["Riyadh", "Jeddah", "Mecca", "Medina", "Dammam", "Khobar", "Abha", "Taif", "Jizan", "Hail"]
+        },
+        {
+            "nation_name":"Japan",
+            "food": [Sushi, Sashimi, Ramen, Tempura, Udon, Miso Soup, Yakitori, Tonkatsu, Okonomiyaki],
+            "culture": [Kimono, Tea Ceremony , Ikebana , Calligraphy , Martial Arts , Noh, Kabuki, Sumo Wrestling, Hanami, Anime and Manga],
+            "structure": [Tokyo Tower, Kyoto Imperial Palace, Himeji Castle, Senso-ji Temple (Tokyo), Osaka Castle, Fushimi Inari Taisha (Kyoto), Tokyo Skytree, Meiji Shrine (Tokyo), Kinkaku-ji (Golden Pavilion, Kyoto), Hiroshima Peace Memorial (Genbaku Dome)],
+            "city_name": [Tokyo, Kyoto, Osaka, Hiroshima, Sapporo, Nagoya, Fukuoka, Yokohama, Nara, Kobe]
+        },
+        {
+            "nation_name": "Philippines",
+            "food": [ Adobo, Sinigang, Lechon, Kare-Kare, Balut, Lumpia, Chicken Adobo, Halo-Halo, Pancit, Sinigang na Baboy],
+            "culture": [Barong Tagalog , Tinikling, Ati-Atihan Festival, Sinulog Festival, Filipino Martial Arts (Arnis, Eskrima, Kali), Jeepney (Transportation), Bahay Kubo (Traditional House), Baybayin (Ancient Script), Filipino Folk Music (Kundiman), Pahiyas Festival ],
+            "structure": [Manila Cathedral, San Agustin Church, Banaue Rice Terraces, Fort Santiago, Paoay Church, "Magellan's Cross", Taal Basilica, Rizal Monument, The Coconut Palace, People Power Monument],
+            "city_name": [Manila, Quezon City, Cebu City, Davao City, Makati City, Taguig City, Baguio City, Iloilo City, Bacolod City, Cagayan de Oro City]
+        },
+        {
+            "nation_name": "Singapore",
+            "food": [ Hainanese Chicken Rice, Laksa, Char Kway Teow, Hainanese Beef Noodles, Nasi Lemak, Chili Crab, Satay, Bak Kut Teh, Rojak, Kaya Toast with Soft-Boiled Eggs],
+            "culture": [ Peranakan Culture, Chingay Parade, Thaipusam Festival, Lantern Festival (Mid-Autumn Festival), Singapore Arts Festival, Chinese Opera, Traditional Music and Dance, Kampong Glam (Malay Heritage), Little India (Indian Heritage), Chinatown (Chinese Heritage)],
+            "structure": [ Marina Bay Sands, Gardens by the Bay, Singapore Flyer, Raffles Hotel, National Gallery Singapore, Esplanade - Theatres on the Bay, Merlion Park, Buddha Tooth Relic Temple, Sentosa Merlion, Sultan Mosque],
+            "city_name": [Singapore City, Jurong, Orchard Road, Marina Bay, Sentosa Island]
+        },
+        {
+            "nation_name": "Indonesia",
+            "food": [ Nasi Goreng (Indonesian Fried Rice), Rendang (Spicy Beef Stew), Satay (Grilled Skewers with Peanut Sauce), Soto Ayam (Chicken Noodle Soup), Nasi Padang (Minangkabau Rice Dish), Gado-gado (Vegetable Salad with Peanut Sauce), Nasi Uduk (Coconut Rice), Bakso (Meatball Soup), Ayam Betutu (Slow-Cooked Chicken), Martabak (Stuffed Pancake)],
+            "culture": [Wayang Kulit (Leather Puppet Shadow Play), Batik (Traditional Fabric Art), Gamelan Music, Barong Dance (Balinese Traditional Dance), Topeng Dance (Mask Dance), Pencak Silat (Traditional Martial Art), Kecak Dance (Monkey Dance), Baju Kurung (Traditional Clothing), Upacara Adat (Customary Ceremonies), Indonesian Traditional Weddings],
+            "structure": [ Borobudur Temple, Prambanan Temple, Istiqlal Mosque (Jakarta), Tanah Lot Temple, Sultan Ahmed Mosque (Demak), Pura Ulun Danu Bratan, Komodo Island National Park (Includes Komodo Dragon), Jakarta Cathedral, Merdeka Palace (Jakarta), Alun-Alun Kidul (Yogyakarta)],
+            "city_name": [ Jakarta, Surabaya, Bandung, Medan, Semarang, Makassar, Palembang, Bandar Lampung, Denpasar, Yogyakarta]
+        },
+        {
+            "nation_name": "india",
+            "food": [Biryani, Masala Dosa, Butter Chicken, Tandoori Chicken, Rogan Josh, Palak Paneer, Chole Bhature, Chaat, Samosa, Lassi ],
+            "culture": [Bharatanatyam (Classical Dance), Carnatic Music, Diwali (Festival of Lights), Holi (Festival of Colors), Yoga and Meditation, Taj Mahal (Architectural Icon), Ayurveda (Traditional Medicine), Sari (Traditional Clothing), Mehndi (Henna Body Art), Vedas and Upanishads (Ancient Texts) ],
+            "structure": [Taj Mahal (Agra), Jaipur City Palace (Jaipur), Hawa Mahal (Jaipur), Qutub Minar (Delhi), Red Fort (Delhi), Lotus Temple (Delhi), Akshardham Temple (Delhi), Mysore Palace (Mysore), Gateway of India (Mumbai), Victoria Memorial (Kolkata) ],
+            "city_name": [ Delhi, Mumbai (Bombay), Bangalore, Chennai, Kolkata (Calcutta), Hyderabad, Pune, Ahmedabad, Jaipur, Lucknow]
+        },
+        {
+            "nation_name": "china",
+            "food": [Zhajiangmian, Galbitang, Dim Sum, Chingdao Bufan, Kuobaro, Mapo Tofu, Hong Kong-Style Dim Sum, Sichuan Cuisine, Yangzhou Fried Rice, Jianjiaoguo Dajiulou ],
+            "culture": [Confucianism, Taoism, Chinese Calligraphy, Chinese Opera, Traditional Tea Ceremony, Martial Arts, Lantern Festival, Dragon Boat Festival, Traditional Chinese Medicine, Feng Shui],
+            "structure": [Great Wall of China, Forbidden City, Temple of Heaven, Summer Palace, Terracotta Army, Potala Palace, Jin Mao Tower, Shanghai World Financial Center, The Bund, West Lake Cultural ],
+            "city_name": [Beijing, Shanghai, Guangzhou, Shenzhen, Chengdu, "Xi'an", Hangzhou, Nanjing, Wuhan, Chongqing ]
+        },
+    ]
+}
